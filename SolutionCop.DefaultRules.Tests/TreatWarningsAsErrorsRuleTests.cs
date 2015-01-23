f243e010ae6fed39ed3995387aab73935cc17c21
Andrey Lipatkin <lipatkin@gmail.com>
2015-01-23 23:50:29 +0300

    New check added for 'Treat warnings as errors'

---
+++ SolutionCop.DefaultRules.Tests/TreatWarningsAsErrorsRuleTests.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace SolutionCop.DefaultRules.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class TreatWarningsAsErrorsRuleTests
+    {
+        private readonly TreatWarningsAsErrorsRule _instance;
+
+        public TreatWarningsAsErrorsRuleTests()
+        {
+            _instance = new TreatWarningsAsErrorsRule();
+        }
+
+        [Fact]
+        public void Should_accept_correct_project()
+        {
+            const string parameters = "<TreatWarningsAsErrors/>";
+            var errors = _instance.ValidateProject(new FileInfo(@"..\..\Data\TreatWarningsAsErrorsInAllConfigurations.csproj").FullName, XElement.Parse(parameters));
+            Assert.Empty(errors);
+        }
+
+        [Fact]
+        public void Should_fail_for_warnings_not_enabled()
+        {
+            const string parameters = "<TreatWarningsAsErrors/>";
+            Approvals.VerifyAll(_instance.ValidateProject(new FileInfo(@"..\..\Data\TreatWarningsAsErrorsDisabled.csproj").FullName, XElement.Parse(parameters)), "Errors");
+        }
+
+        [Fact]
+        public void Should_fail_for_warnings_enabled_in_one_configuration_only()
+        {
+            const string parameters = "<TreatWarningsAsErrors/>";
+            Approvals.VerifyAll(_instance.ValidateProject(new FileInfo(@"..\..\Data\TreatWarningsAsErrorsInOneConfigurationOnly.csproj").FullName, XElement.Parse(parameters)), "Errors");
+        }
+
+        [Fact]
+        public void Should_skip_disabled_rule()
+        {
+            const string parameters = "<TreatWarningsAsErrors enabled=\"false\"></TreatWarningsAsErrors>";
+            var errors = _instance.ValidateProject(new FileInfo(@"..\..\Data\TreatWarningsAsErrorsInOneConfigurationOnly.csproj").FullName, XElement.Parse(parameters));
+            Assert.Empty(errors);
+        }
+    }
+}
---
+++ SolutionCop.DefaultRules/TreatWarningsAsErrorsRule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SolutionCop.DefaultRules
+{
+    public class TreatWarningsAsErrorsRule : StandardProjectRule
+    {
+        public override string DisplayName
+        {
+            get { return "Verify that all warnings are treated as errors"; }
+        }
+
+        public override string Id
+        {
+            get { return "TreatWarningsAsErrors"; }
+        }
+
+        protected override IEnumerable<string> ValidateProjectWithEnabledRule(string projectFilePath, XElement xmlRuleParameters, XDocument xmlProject)
+        {
+            var xmlPropertyGroupsWithConditions = xmlProject.Descendants(Namespace + "PropertyGroup").Where(x => x.Attribute("Condition") != null);
+            foreach (var xmlPropertyGroupsWithCondition in xmlPropertyGroupsWithConditions)
+            {
+                var xmlTreatWarningsAsErrors = xmlPropertyGroupsWithCondition.Descendants(Namespace + "TreatWarningsAsErrors").FirstOrDefault();
+                if (xmlTreatWarningsAsErrors == null || xmlTreatWarningsAsErrors.Value != "true")
+                {
+                    return Enumerable.Repeat(string.Format("'Treat warnings as errors' is not enabled in project: {0}", Path.GetFileName(projectFilePath)), 1);
+                }
+            }
+            return Enumerable.Empty<string>();
+        }
+    }
+}
