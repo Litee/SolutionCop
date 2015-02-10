f2c495a7702f322c5a8c57df59a7a180b4c51722
Andrey Lipatkin <lipatkin@gmail.com>
2015-02-10 22:18:01 +0300

    Added support for file patterns to FilesIncludedIntoProjectRule

--- src/SolutionCop.DefaultRules.Tests/FilesIncludedIntoProjectRuleTests.cs
+++ src/SolutionCop.DefaultRules.Tests/FilesIncludedIntoProjectRuleTests.cs
@@ -17,14 +17,30 @@ namespace SolutionCop.DefaultRules.Tests
         [Fact]
         public void Should_pass_if_all_files_are_included()
         {
-            var xmlConfig = XElement.Parse("<FilesIncludedIntoProject/>");
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+</FilesIncludedIntoProject>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\AllFilesIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_pass_if_files_do_not_match_search_pattern()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.txt</FileName>
+</FilesIncludedIntoProject>");
             ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\AllFilesIncludedIntoProject.csproj").FullName);
         }
 
         [Fact]
         public void Should_fail_if_file_is_not_included()
         {
-            var xmlConfig = XElement.Parse("<FilesIncludedIntoProject/>");
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+</FilesIncludedIntoProject>");
             ShouldFailNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
         }
 
@@ -33,6 +49,7 @@ namespace SolutionCop.DefaultRules.Tests
         {
             var xmlConfig = XElement.Parse(@"
 <FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
   <Exception><Project>SomeNonExistingProject.csproj</Project></Exception>
   <Exception><Project>FileNotIncludedIntoProject.csproj</Project></Exception>
 </FilesIncludedIntoProject>");
@@ -40,10 +57,80 @@ namespace SolutionCop.DefaultRules.Tests
         }
 
         [Fact]
+        public void Should_pass_if_file_name_is_an_exception()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+  <Exception><Project>SomeNonExistingProject.csproj</Project></Exception>
+  <Exception><FileName>My.File.cs</FileName></Exception>
+  <Exception><FileName>AssemblyInfo.cs</FileName></Exception>
+</FilesIncludedIntoProject>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_pass_if_file_name_with_wildcard_is_an_exception()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+  <Exception><Project>SomeNonExistingProject.csproj</Project></Exception>
+  <Exception><FileName>My.*.cs</FileName></Exception>
+  <Exception><FileName>AssemblyInfo.cs</FileName></Exception>
+</FilesIncludedIntoProject>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_pass_if_both_project_and_file_name_with_wildcard_is_an_exception()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+  <Exception>
+    <Project>FileNotIncludedIntoProject.csproj</Project>
+    <FileName>My.*.cs</FileName>
+    <FileName>AssemblyInfo.cs</FileName>
+  </Exception>
+</FilesIncludedIntoProject>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_pass_if_both_project_and_file_name_with_wildcard_is_an_exception_plus_global_file_exception()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+  <Exception>
+    <Project>FileNotIncludedIntoProject.csproj</Project>
+    <FileName>My.*.cs</FileName>
+  </Exception>
+  <Exception>
+    <FileName>AssemblyInfo.cs</FileName>
+  </Exception>
+</FilesIncludedIntoProject>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_fail_if_file_name_is_an_exception_but_for_another_project()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
+  <Exception><Project>SomeNonExistingProject.csproj</Project><FileName>My.File.cs</FileName><FileName>AssemblyInfo.cs</FileName></Exception>
+</FilesIncludedIntoProject>");
+            ShouldFailNormally(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
         public void Should_fail_if_exception_misses_project()
         {
             var xmlConfig = XElement.Parse(@"
 <FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
   <Exception>Some text</Exception>
   <Exception><Project>FileNotIncludedIntoProject.csproj</Project></Exception>
 </FilesIncludedIntoProject>");
@@ -55,8 +142,18 @@ namespace SolutionCop.DefaultRules.Tests
         {
             var xmlConfig = XElement.Parse(@"
 <FilesIncludedIntoProject>
+  <FileName>*.cs</FileName>
   <Dummy>Some text</Dummy>
   <Exception><Project>FileNotIncludedIntoProject.csproj</Project></Exception>
+</FilesIncludedIntoProject>");
+            ShouldFailOnConfiguration(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_fail_if_no_search_pattern()
+        {
+            var xmlConfig = XElement.Parse(@"
+<FilesIncludedIntoProject>
 </FilesIncludedIntoProject>");
             ShouldFailOnConfiguration(xmlConfig, new FileInfo(@"..\..\Data\FilesIncludedIntoProject\FileNotIncludedIntoProject.csproj").FullName);
         }
--- src/SolutionCop.DefaultRules/FilesIncludedIntoProjectRule.cs
+++ src/SolutionCop.DefaultRules/FilesIncludedIntoProjectRule.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
 namespace SolutionCop.DefaultRules
 {
-    public class FilesIncludedIntoProjectRule : ProjectRule<string[]>
+    public class FilesIncludedIntoProjectRule : ProjectRule<FilesIncludedIntoProjectRuleConfig>
     {
         public override string DisplayName
         {
@@ -24,54 +23,108 @@ namespace SolutionCop.DefaultRules
             {
                 var element = new XElement(Id);
                 element.SetAttributeValue("enabled", "false");
+                element.Add(new XElement("FileName", "*.cs"));
                 element.Add(new XElement("Exception", new XElement("Project", "ProjectToExcludeFromCheck.csproj")));
+                element.Add(new XElement("Exception", new XElement("FileName", "*.approved.cs")));
                 return element;
             }
         }
 
-        protected override string[] ParseConfigurationSection(XElement xmlRuleConfigs, List<string> errors)
+        protected override FilesIncludedIntoProjectRuleConfig ParseConfigurationSection(XElement xmlRuleConfigs, List<string> errors)
         {
-            var unknownElements = xmlRuleConfigs.Elements().Select(x => x.Name.LocalName).Where(x => x != "Exception").ToArray();
+            var unknownElements = xmlRuleConfigs.Elements().Select(x => x.Name.LocalName).Where(x => x != "Exception" && x != "FileName").ToArray();
             if (unknownElements.Any())
             {
                 errors.Add(string.Format("Bad configuration for rule {0}: Unknown element(s) {1} in configuration.", Id, string.Join(",", unknownElements)));
             }
-            foreach (var xmlException in xmlRuleConfigs.Elements("Exception"))
-            {
-                var xmlProject = xmlException.Element("Project");
-                if (xmlProject == null)
+            var config = new FilesIncludedIntoProjectRuleConfig();
+            config.FilePatternsToProcess.AddRange(xmlRuleConfigs.Elements("FileName").Select(x => x.Value.Trim()));
+            if (config.FilePatternsToProcess.Any())
+                foreach (var xmlException in xmlRuleConfigs.Elements("Exception"))
                 {
-                    errors.Add(string.Format("Bad configuration for rule {0}: <Project> element is missing in exceptions list.", Id));
+                    var xmlProject = xmlException.Element("Project");
+                    var xmlFile = xmlException.Element("FileName");
+                    if (xmlProject == null && xmlFile == null)
+                    {
+                        errors.Add(string.Format("Bad configuration for rule {0}: <Project> or <FileName> element is missing in exceptions list.", Id));
+                    }
+                    else if (xmlProject != null)
+                    {
+                        config.ProjectSpecificFilePatternExceptions.Add(xmlProject.Value.Trim(), xmlException.Elements("FileName").Select(x => x.Value.Trim()).ToArray());
+                    }
+                    else
+                    {
+                        config.GlobalFilePatternExceptions.AddRange(xmlException.Elements("FileName").Select(x => x.Value.Trim()));
+                    }
                 }
-            }
-            return xmlRuleConfigs.Elements("Exception").Select(x => x.Value.Trim()).ToArray();
-        }
-
-        protected override IEnumerable<string> ValidateSingleProject(XDocument xmlProject, string projectFilePath, string[] exceptions)
-        {
-            var projectFileName = Path.GetFileName(projectFilePath);
-            if (exceptions.Contains(projectFileName))
-            {
-                Console.Out.WriteLine("DEBUG: Skipping project as an exception: {0}", projectFileName);
-            }
             else
             {
-                // TODO Support other extensions
-                var filePaths = Directory.EnumerateFiles(Path.GetDirectoryName(projectFilePath), "*.cs", SearchOption.AllDirectories);
+                errors.Add(string.Format("Bad configuration for rule {0}: No file names to process.", Id));
+            }
+            return config;
+        }
+
+        protected override IEnumerable<string> ValidateSingleProject(XDocument xmlProject, string projectFilePath, FilesIncludedIntoProjectRuleConfig config)
+        {
+            var projectFileName = Path.GetFileName(projectFilePath);
+            var filePatternsToExclude = new List<string>(config.GlobalFilePatternExceptions);
+            if (config.ProjectSpecificFilePatternExceptions.ContainsKey(projectFileName))
+            {
+                var fileExceptions = config.ProjectSpecificFilePatternExceptions[projectFileName];
+                if (fileExceptions.Any())
+                {
+                    filePatternsToExclude.AddRange(fileExceptions);
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+            foreach (var filePatternToProcess in config.FilePatternsToProcess)
+            {
+                var filePaths = Directory.EnumerateFiles(Path.GetDirectoryName(projectFilePath), filePatternToProcess, SearchOption.AllDirectories);
+                // TODO More flexible folder exclusions
                 foreach (var filePath in filePaths.Where(x => !x.ToLower().Contains(@"\obj\") && !x.ToLower().Contains(@"\bin\")))
                 {
-                    var fileName = Path.GetFileName(filePath);
-                    // TODO Make more precise
-                    var xmlHintPaths = xmlProject.Descendants(Namespace + "Compile").Where(x =>
+                    var parentDir = Path.GetDirectoryName(filePath);
+                    // TODO Yak!
+                    var matchesExcludeFilePattern = filePatternsToExclude.Any(fileToExclude => Directory.EnumerateFiles(parentDir, fileToExclude).Any(x => x == filePath));
+                    if (!matchesExcludeFilePattern)
                     {
-                        return x.Attribute("Include").Value.Contains(fileName);
-                    });
-                    if (!xmlHintPaths.Any())
-                    {
-                        yield return string.Format("File is not referenced in project: {0}", fileName);
+                        var fileName = Path.GetFileName(filePath);
+                        // TODO Make more precise
+                        var xmlHintPaths = xmlProject.Descendants(Namespace + "Compile").Where(x => x.Attribute("Include").Value.Contains(fileName));
+                        if (!xmlHintPaths.Any())
+                        {
+                            // TODO Yak!
+                            var fileSubPath = Path.GetFullPath(filePath).Substring(Path.GetDirectoryName(projectFilePath).Length + 1);
+                            yield return string.Format("File is not referenced in project: {0}", fileSubPath);
+                        }
                     }
                 }
             }
+        }
+    }
+
+    public class FilesIncludedIntoProjectRuleConfig
+    {
+        private readonly List<string> _filePatternsToProcess = new List<string>();
+        private readonly Dictionary<string, string[]> _projectSpecificFilePatternExceptions = new Dictionary<string, string[]>();
+        private readonly List<string> _globalFilePatternExceptions = new List<string>();
+
+        public List<string> FilePatternsToProcess
+        {
+            get { return _filePatternsToProcess; }
+        }
+
+        public Dictionary<string, string[]> ProjectSpecificFilePatternExceptions
+        {
+            get { return _projectSpecificFilePatternExceptions; }
+        }
+
+        public List<string> GlobalFilePatternExceptions
+        {
+            get { return _globalFilePatternExceptions; }
         }
     }
 }
