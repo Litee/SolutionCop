97d7303c0f2b0a4bcdad0f28877f0a9598d574ae
Andrey Lipatkin <lipatkin@gmail.com>
2015-02-12 23:22:16 +0300

    First dummy version of SolutionCop.MSBuild

---
+++ src/SolutionCop.MSBuild/SolutionCopTask.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+using SolutionCop.DefaultRules.Basic;
+
+namespace SolutionCop.MSBuild
+{
+    using System.Diagnostics;
+    using Microsoft.Build.Utilities;
+
+    public class SolutionCopTask : Task
+    {
+        static SolutionCopTask()
+        {
+#if DEBUG
+            // Add the Default Listener back as ReSharper removes it
+            Trace.Listeners.Add(new DefaultTraceListener());
+#endif
+        }
+
+        public string PathToConfig { get; set; }
+
+        public override bool Execute()
+        {
+            var validationResult = new WarningLevelRule().ValidateAllProjects(new XElement("WarningLevel"), PathToConfig);
+            if (validationResult.Errors.Length > 0)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    Log.LogError("SolutionCop: " + error);
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
