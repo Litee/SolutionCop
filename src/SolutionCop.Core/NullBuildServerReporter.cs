dffe4c6539c29f64a8e0a6863ffef920cf586157
skurdiukov <skurdiukov@esphere.ru>
2016-04-20 13:26:39 +0300

    Threat each rule as a separate test for TeamCity

---
+++ src/SolutionCop.Core/NullBuildServerReporter.cs
@@ -0,0 +1,32 @@
+﻿namespace SolutionCop.Core
+{
+    /// <summary>
+    /// Null build server reporter.
+    /// </summary>
+    public class NullBuildServerReporter : IBuildServerReporter
+    {
+        public void TestSuiteStarted(string suiteName)
+        {
+        }
+
+        public void TestSuiteFinished(string suiteName)
+        {
+        }
+
+        public void TestIgnored(string testName)
+        {
+        }
+
+        public void TestStarted(string testName)
+        {
+        }
+
+        public void TestFinished(string testName)
+        {
+        }
+
+        public void TestFailed(string testName, string message, string details)
+        {
+        }
+    }
+}
