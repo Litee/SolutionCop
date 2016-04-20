e52a996ccf12bccf78279368d063b21d9c55a0ea
skurdiukov <skurdiukov@esphere.ru>
2016-04-20 13:26:39 +0300

    Treat each rule as a separate test for TeamCity

--- src/SolutionCop.CommandLine/Program.cs
+++ src/SolutionCop.CommandLine/Program.cs
@@ -42,30 +42,34 @@ namespace SolutionCop.CommandLine
                     SolutionCopConsole.LogInfo("Custom path to config file is not specified, using default one: {0}", pathToConfigFile);
                 }
 
+                var reporter = CreateBuildServerReporter(commandLineParameters);
 
-                var verificationResult = new ProjectsVerifier(SolutionCopConsole).VerifyProjects(pathToConfigFile, solutionInfo.ProjectFilePaths.ToArray(), (errors, validationResults) =>
-                {
-                    if (errors.Any())
-                    {
-                        SolutionCopConsole.LogError("***** Full list of errors: *****");
-                        errors.ForEach(x => SolutionCopConsole.LogError(x));
-                        if (commandLineParameters.BuildServerType == BuildServer.TeamCity)
+                var verificationResult = new ProjectsVerifier(SolutionCopConsole, reporter).VerifyProjects(
+                    pathToConfigFile,
+                    solutionInfo.ProjectFilePaths.ToArray(), (errors, validationResults) =>
                         {
-                            // adding empty line for a better formatting in TC output
-                            var extendedErrorsInfo = Enumerable.Repeat(string.Empty, 1).Concat(errors.Select((x, idx) => string.Format("ERROR ({0}/{1}): {2}", idx + 1, errors.Count, x))).Concat(Enumerable.Repeat(String.Empty, 1)).Concat(validationResults.Select(x => String.Format("INFO: Rule {0} is {1}", x.RuleId, x.IsEnabled ? "enabled" : "disabled")));
-                            Console.Out.WriteLine("##teamcity[testFailed name='SolutionCop' message='FAILED - {0}' details='{1}']", EscapeForTeamCity(Path.GetFileName(pathToConfigFile)), string.Join("|r|n", extendedErrorsInfo.Select(EscapeForTeamCity)));
-                            Console.Out.WriteLine("##teamcity[buildStatus text='FAILED - {0}']", EscapeForTeamCity(Path.GetFileName(pathToConfigFile)));
-                        }
-                    }
-                    else
-                    {
-                        SolutionCopConsole.LogInfo("No errors found!");
-                        if (commandLineParameters.BuildServerType == BuildServer.TeamCity && !commandLineParameters.BuildServerNoSuccessMessages)
-                        {
-                            Console.Out.WriteLine("##teamcity[buildStatus status='SUCCESS' text='PASSED - {0}']", EscapeForTeamCity(Path.GetFileName(pathToConfigFile)));
-                        }
-                    }
-                });
+                            if (errors.Any())
+                            {
+                                SolutionCopConsole.LogError("***** Full list of errors: *****");
+                                errors.ForEach(x => SolutionCopConsole.LogError(x));
+                                if (commandLineParameters.BuildServerType == BuildServer.TeamCity)
+                                {
+                                    // adding empty line for a better formatting in TC output
+                                    // var extendedErrorsInfo = Enumerable.Repeat(string.Empty, 1).Concat(errors.Select((x, idx) => string.Format("ERROR ({0}/{1}): {2}", idx + 1, errors.Count, x))).Concat(Enumerable.Repeat(String.Empty, 1)).Concat(validationResults.Select(x => String.Format("INFO: Rule {0} is {1}", x.RuleId, x.IsEnabled ? "enabled" : "disabled")));
+                                    // Console.Out.WriteLine("##teamcity[testFailed name='SolutionCop' message='FAILED - {0}' details='{1}']", EscapeForTeamCity(Path.GetFileName(pathToConfigFile)), string.Join("|r|n", extendedErrorsInfo.Select(EscapeForTeamCity)));
+                                    TeamCityReporter.WriteCommand("buildProblem", new KeyValuePair<string, string>("description", String.Concat("FAILED - ", Path.GetFileName(pathToConfigFile))));
+                                }
+                            }
+                            else
+                            {
+                                SolutionCopConsole.LogInfo("No errors found!");
+
+                                if (commandLineParameters.BuildServerType == BuildServer.TeamCity && !commandLineParameters.BuildServerNoSuccessMessages)
+                                {
+                                    TeamCityReporter.WriteCommand("progressMessage", new KeyValuePair<string, string>("text", String.Concat("PASSED -", Path.GetFileName(pathToConfigFile))));
+                                }
+                            }
+                        });
                 Environment.Exit(verificationResult == VerificationResult.ErrorsFound ? -1 : 0);
             }
             else
@@ -75,9 +79,19 @@ namespace SolutionCop.CommandLine
             }
         }
 
-        private static string EscapeForTeamCity(string originalString)
+        private static IBuildServerReporter CreateBuildServerReporter(CommandLineParameters commandLineParameters)
         {
-            return originalString.Replace("|", "||").Replace("'", "|'").Replace("\r", "|r").Replace("\n", "|n").Replace("]", "|]");
+            IBuildServerReporter reporter;
+            switch (commandLineParameters.BuildServerType)
+            {
+                case BuildServer.TeamCity:
+                    reporter = new TeamCityReporter();
+                    break;
+                default:
+                    reporter = new NullBuildServerReporter();
+                    break;
+            }
+            return reporter;
         }
     }
 }
---
+++ src/SolutionCop.Core/TeamCityReporter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionCop.Core
+{
+    using Arg = KeyValuePair<string, string>;
+
+    public class TeamCityReporter : IBuildServerReporter
+    {
+        #region Implementation of IBuildServerReporter
+
+        /// <summary>
+        /// Report about started test suite.
+        /// </summary>
+        /// <param name="suiteName">Test suite name.</param>
+        public void TestSuiteStarted(string suiteName)
+        {
+            WriteCommand("testSuiteStarted", new Arg("name", suiteName));
+        }
+
+        /// <summary>
+        /// Report about finished test suite.
+        /// </summary>
+        /// <param name="suiteName">Test suite name.</param>
+        public void TestSuiteFinished(string suiteName)
+        {
+            WriteCommand("testSuiteFinished", new Arg("name", suiteName));
+        }
+
+        /// <summary>
+        /// Report about ignored test.
+        /// </summary>
+        /// <param name="testName">Test name.</param>
+        public void TestIgnored(string testName)
+        {
+            WriteCommand("testIgnored", new Arg("name", testName));
+        }
+
+        /// <summary>
+        /// Report about started test.
+        /// </summary>
+        /// <param name="testName">Test name.</param>
+        public void TestStarted(string testName)
+        {
+            WriteCommand("testStarted", new Arg("name", testName));
+        }
+
+        /// <summary>
+        /// Report about finished test.
+        /// </summary>
+        /// <param name="testName">Test name.</param>
+        public void TestFinished(string testName)
+        {
+            WriteCommand("testFinished", new Arg("name", testName));
+        }
+
+        /// <summary>
+        /// Report about failed test.
+        /// </summary>
+        /// <param name="testName">Test name.</param>
+        /// <param name="message">Textual representation of the error.</param>
+        /// <param name="details">Detailed information on the test failure, typically a message and an exception stacktrace.</param>
+        public void TestFailed(string testName, string message, string details)
+        {
+            WriteCommand(
+                "testFailed",
+                new Arg("name", testName),
+                new Arg("message", message),
+                new Arg("details", details));
+        }
+
+        #endregion
+
+        public static void WriteCommand(string command, params Arg[] args)
+        {
+            Console.Out.Write("##teamcity[");
+            Console.Out.Write(command);
+
+            foreach (var arg in args)
+            {
+                Console.Out.Write(" {0}='{1}'", arg.Key, EscapeForTeamCity(arg.Value));
+            }
+
+            Console.Out.WriteLine("]");
+        }
+
+        private static string EscapeForTeamCity(string originalString)
+        {
+            return originalString.Replace("|", "||").Replace("'", "|'").Replace("\r", "|r").Replace("\n", "|n").Replace("]", "|]");
+        }
+    }
+}
