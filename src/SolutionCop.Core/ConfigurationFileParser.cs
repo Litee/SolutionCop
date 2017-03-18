d5534554c73a179f2a29d983fa4b947ef1217f5d
Andrey Lipatkin <lipatkin@gmail.com>
2017-03-18 18:14:42 +0300

    Moved schema file into embedded resource

--- src/SolutionCop.Core/ConfigurationFileParser.cs
+++ src/SolutionCop.Core/ConfigurationFileParser.cs
@@ -10,6 +10,7 @@
 
     public class ConfigurationFileParser
     {
+        private const string SolutionCopXsdFileName = "SolutionCop.xsd";
         private readonly XNamespace _xsi = XNamespace.Get("http://www.w3.org/2001/XMLSchema-instance");
         private readonly Action<string, byte[]> _saveConfigFileAction;
         private readonly ISolutionCopConsole _logger;
@@ -40,7 +41,7 @@
             }
         }
 
-        public Dictionary<string, XElement> ParseConfigString(string pathToConfigFile, string rulesConfiguration, IEnumerable<IProjectRule> rules, List<string> errors)
+        public Dictionary<string, XElement> ParseConfigString(string pathToConfigFile, string rulesConfiguration, IEnumerable<IProjectRule> knownRules, List<string> errors)
         {
             try
             {
@@ -54,18 +55,23 @@
                 }
                 else
                 {
-                    if (!xmlRules.Attributes().Any(x => x.Value == "http://www.w3.org/2001/XMLSchema-instance"))
+                    if (xmlRules.Attributes().All(x => x.Value != "http://www.w3.org/2001/XMLSchema-instance"))
                     {
                         xmlRules.Add(new XAttribute(XNamespace.Xmlns + "xsi", _xsi));
-                        xmlRules.Add(new XAttribute(_xsi + "noNamespaceSchemaLocation", "SolutionCop.xsd"));
+                        xmlRules.Add(new XAttribute(_xsi + "noNamespaceSchemaLocation", SolutionCopXsdFileName));
                         saveConfigFileOnExit = true;
-                        var pathToSchemaFile = Path.GetDirectoryName(pathToConfigFile) + Path.DirectorySeparatorChar + "SolutionCop.xsd";
+                        var pathToSchemaFile = Path.GetDirectoryName(pathToConfigFile) + Path.DirectorySeparatorChar + SolutionCopXsdFileName;
                         if (!File.Exists(pathToSchemaFile))
                         {
-                            File.Copy("SolutionCop.xsd", pathToSchemaFile);
+                            using (var stream = typeof(ConfigurationFileParser).Assembly.GetManifestResourceStream("SolutionCop.Core." + SolutionCopXsdFileName))
+                            {
+                                byte[] buffer = new byte[stream.Length];
+                                stream.Read(buffer, 0, buffer.Length);
+                                File.WriteAllBytes(pathToSchemaFile, buffer);
+                            }
                         }
                     }
-                    foreach (var rule in rules)
+                    foreach (var rule in knownRules)
                     {
                         var xmlRuleConfig = xmlRules.Element(rule.Id);
                         if (xmlRuleConfig == null)
@@ -82,7 +88,7 @@
                             ruleConfigsMap.Add(rule.Id, xmlRuleConfig);
                         }
                     }
-                    var unknownSectionNames = xmlRules.Elements().Select(x => x.Name.LocalName).Except(rules.Select(x => x.Id));
+                    var unknownSectionNames = xmlRules.Elements().Select(x => x.Name.LocalName).Except(knownRules.Select(x => x.Id));
                     foreach (var unknownSectionName in unknownSectionNames)
                     {
                         _logger.LogWarning("Unknown configuration section {0}", unknownSectionName);
