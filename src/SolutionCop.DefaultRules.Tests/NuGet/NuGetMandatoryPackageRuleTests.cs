6865b66b95ed5fac9d272d30677a7972c4bc06e6
Andrey Lipatkin <lipatkin@gmail.com>
2017-03-25 17:07:27 +0300

    Added new NuGetMandatoryPackage rule

---
+++ src/SolutionCop.DefaultRules.Tests/NuGet/NuGetMandatoryPackageRuleTests.cs
@@ -0,0 +1,94 @@
+﻿namespace SolutionCop.DefaultRules.Tests.NuGet
+{
+    using System.IO;
+    using System.Xml.Linq;
+    using ApprovalTests.Namers;
+    using ApprovalTests.Reporters;
+    using DefaultRules.NuGet;
+    using Xunit;
+
+    [UseReporter(typeof(DiffReporter))]
+    [UseApprovalSubdirectory("ApprovedResults")]
+    public class NuGetMandatoryPackageRuleTests : ProjectRuleTest
+    {
+        public NuGetMandatoryPackageRuleTests()
+            : base(new NuGetMandatoryPackageRule())
+        {
+        }
+
+        [Fact]
+        public void Should_pass_if_mandatory_package_exists()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage>
+  <Package id='ApprovalTests'></Package>
+</NuGetMandatoryPackage>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_1\Project.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_fail_if_mandatory_package_is_not_used()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage>
+  <Package id='MissingPackage'></Package>
+</NuGetMandatoryPackage>");
+            ShouldFailNormally(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_1\Project.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_fail_if_no_packages_used_at_all()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage>
+  <Package id='ApprovalTests'></Package>
+</NuGetMandatoryPackage>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_2\UsesNoPackages.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_pass_if_version_does_not_match_the_rule_but_project_is_an_exception()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage>
+  <Package id='MissingPackage'></Package>
+  <Exception>
+    <Project>Project.csproj</Project>
+  </Exception>
+</NuGetMandatoryPackage>");
+            ShouldPassNormally(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_1\Project.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_fail_if_project_is_missing_in_exception()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage>
+  <Package id='ApprovalTests'></Package>
+  <Exception>Some text</Exception>
+</NuGetMandatoryPackage>");
+            ShouldFailOnConfiguration(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_1\Project.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_fail_if_unknown_element_in_config()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage>
+  <Dummy></Dummy>
+  <Package id='ApprovalTests'></Package>
+</NuGetMandatoryPackage>");
+            ShouldFailOnConfiguration(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_1\Project.csproj").FullName);
+        }
+
+        [Fact]
+        public void Should_pass_if_rule_is_disabled()
+        {
+            var xmlConfig = XElement.Parse(@"
+<NuGetMandatoryPackage enabled='false'>
+  <Package id='MissingPackage'></Package>
+</NuGetMandatoryPackage>");
+            ShouldPassAsDisabled(xmlConfig, new FileInfo(@"..\..\Data\NuGetMandatoryPackage_1\Project.csproj").FullName);
+        }
+    }
+}
---
+++ src/SolutionCop.DefaultRules/NuGet/NuGetMandatoryPackageRule.cs
@@ -0,0 +1,85 @@
+﻿namespace SolutionCop.DefaultRules.NuGet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+    using Core;
+    using global::NuGet;
+
+    [Export(typeof(IProjectRule))]
+    public class NuGetMandatoryPackageRule : ProjectRule<Tuple<List<XElement>, string[]>>
+    {
+        public override string Id
+        {
+            get { return "NuGetMandatoryPackage"; }
+        }
+
+        public override XElement DefaultConfig
+        {
+            get
+            {
+                var element = new XElement(Id);
+                element.SetAttributeValue("enabled", "false");
+                element.Add(new XComment("This package must be referenced in all projects"));
+                element.Add(new XElement("Package", new XAttribute("id", "mandatory-package-id")));
+                element.Add(new XComment("SomeProject.csproj does not have to reference mandatory packages"));
+                element.Add(new XElement("Exception", new XElement("Project", "SomeProject.csproj")));
+                return element;
+            }
+        }
+
+        protected override Tuple<List<XElement>, string[]> ParseConfigurationSection(XElement xmlRuleConfigs, List<string> errors)
+        {
+            ValidateConfigSectionForAllowedElements(xmlRuleConfigs, errors, "Exception", "Package");
+            foreach (var xmlException in xmlRuleConfigs.Elements("Exception"))
+            {
+                var xmlProject = xmlException.Element("Project");
+                if (xmlProject == null)
+                {
+                    errors.Add($"Bad configuration for rule {Id}: <Project> element is missing in exceptions list.");
+                }
+            }
+
+            var exceptionProjects = xmlRuleConfigs.Elements("Exception").Select(x => x.Value.Trim()).ToArray();
+            var xmlPackageRules = xmlRuleConfigs.Elements().Where(x => x.Name.LocalName == "Package");
+            var packageRules = new List<XElement>();
+            foreach (var xmlPackageRule in xmlPackageRules)
+            {
+                packageRules.Add(xmlPackageRule);
+            }
+
+            return Tuple.Create(packageRules, exceptionProjects);
+        }
+
+        protected override IEnumerable<string> ValidateSingleProject(XDocument xmlProject, string projectFilePath, Tuple<List<XElement>, string[]> ruleConfiguration)
+        {
+            var xmlPackageRules = ruleConfiguration.Item1;
+            var exceptions = ruleConfiguration.Item2;
+            var projectFileName = Path.GetFileName(projectFilePath);
+            if (exceptions.Contains(projectFileName))
+            {
+                Console.Out.WriteLine("DEBUG: Skipping project with disabled StyleCop as an exception: {0}", Path.GetFileName(projectFilePath));
+            }
+            else
+            {
+                var pathToPackagesConfigFile = Path.Combine(Path.GetDirectoryName(projectFilePath), "packages.config");
+                if (File.Exists(pathToPackagesConfigFile))
+                {
+                    var xmlUsedPackages = XDocument.Load(pathToPackagesConfigFile).Element("packages").Elements("package");
+                    foreach (var xmlPackageRule in xmlPackageRules)
+                    {
+                        var mandatoryPackageId = xmlPackageRule.Attribute("id").Value;
+                        var matchingPackageInProject = xmlUsedPackages.FirstOrDefault(x => x.Attribute("id").Value == mandatoryPackageId);
+                        if (matchingPackageInProject == null)
+                        { // mandatory package was not found
+                            yield return $"Missing mandatory package '{mandatoryPackageId}' in project {projectFileName}";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
